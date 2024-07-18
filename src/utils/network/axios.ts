e6b5ed2d05b7a599d338505f650b8058fa0e771b
Patricia Meta <metapatricia251@gmail.com>
2024-07-19 02:03:19 +0700

    adding Docker file & docker image yml

--- src/utils/network/axios.ts
+++ src/utils/network/axios.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const headers = {
   accept: "application/json",
-  Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwM2M5OWQ1OWI1N2FhY2YxZTc2NmJjNzg3ZDQzYTlhMyIsInN1YiI6IjY0YmQyM2JmZWI3OWMyMDBjNWRhMjY4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.03XEmHZDdjsPt-Edsc320Zwa-fFNg4aKRrzCqYSYDy8`,
+  Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY} `,
 };
 
 const apiMovie = axios.create({
-  baseURL: "https://api.themoviedb.org/3/movie",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers,
 });
 
